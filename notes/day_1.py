e079629d8a2f7a7d91b8dd5f4f3f09ed862653ed
Mauricio Degregori <degregori2@gmail.com>
2020-05-19 01:14:03 -0500

    class notes

--- notes/day_1.py
+++ notes/day_1.py
@@ -57,9 +57,70 @@ Byte = 8 bits
 
 Byte example = 11011011
 conversion into hex -> base 16
-1101 1011
+1101 0011
 
-(1+0+)
+13+3
+d 3
+
+0b11010011 = 0xd3
+
+list can be your memory.
+
+
+ 
 
 
 """
+
+# Write a program that runs programs
+
+PRINT_BEEJ = 1
+HALT = 2
+SAVE_REG = 3 # store a value in a register (in the LS8 called LDI)
+PRINT_REG = 4 #corresponds to PRN in the LS8
+
+#RAM -> random access memory -> list makes a lot of sense here
+memory = [
+    PRINT_BEEJ,
+    SAVE_REG,   # SAVE R0, 37 store 37 in RO
+    0, # R0 operand ("argument")
+    37,# 37 operand 
+    PRINT_BEEJ,
+    PRINT_REG,
+    0, #R0
+
+    HALT
+]
+# a byte (?) -> 8 bits
+register = [0] * 8 # like variables, R0-R7
+
+# adds flexibility instead of for loop
+pc = 0
+# what index in memory is it?
+running = True
+
+while running:
+    inst = memory[pc]
+    if inst == PRINT_BEEJ:
+        print("Beej!")
+        pc += 1
+    elif inst = SAVE_REG:
+        reg_num = memory[pc + 1]
+        value = memory [pc + 2]
+        register[reg_num] = value
+        pc += 3
+    elif inst = PRINT_REG:
+        reg_num = memory[pc + 1]
+        value = register[reg_num]
+        print(value)
+        pc += 2
+    elif inst == HALT:
+        running = False
+    else:
+        print("Unknown Instruction")
+        running = False 
+
+
+# i dont understand the point of the register
+
+
